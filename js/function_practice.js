44c7753492fd6155f40c1ebc77889f572206ca66
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-15 14:53:24 -0500

    finsihed exercise 2

--- js/function_practice.js
+++ js/function_practice.js
@@ -4,6 +4,14 @@ console.log(sample);
 
 console.log(sample.length);
 
+console.log(sample.concat(" Students"))
+
+console.log(sample.indexOf("c"))
+
+console.log(sample.indexOf("C"))
+
+console.log(sample.indexOf("C"), sample.substring())
+
 //
 // console.log(sample.toLowerCase())
 // console.log(sample.toUpperCase())
