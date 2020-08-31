5e8ba9dad52e23781cb3a5af934e477bf032bebf
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-31 18:38:05 -0500

    progress made on loop practice

--- js/function_practice.js
+++ js/function_practice.js
@@ -15,3 +15,18 @@ console.log(sample.indexOf("C"), sample.substring())
 //
 // console.log(sample.toLowerCase())
 // console.log(sample.toUpperCase())
+
+function fizzBuzz(){
+    for(var i = 0; i <= 50; i++) {
+        if (i%15 === 0){
+            console.log(i + ' FizzBuzz')
+        }else if(i%3 === 0){
+            console.log(i + ' Fizz')
+//         }else if(i%5 === 0){
+//             console.log(i + ' Buzz')
+//         }else{
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz()
